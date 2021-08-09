fbc1e197169339bbd59b9406fef8d77ad10a19fc
Kavitha1387 <33650574+Kavitha1387@users.noreply.github.com>
2021-08-09 17:24:58 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"kavitha.a1387@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
